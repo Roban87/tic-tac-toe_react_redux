a61311cd5a5a5201dd018bd4d84a03181192c934
Roban87 <pap.zoltan87@gmail.com>
2021-03-31 17:41:44 +0200

    add persist to redux store

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -2,14 +2,15 @@ import { combineReducers } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
 import { persistReducer } from 'redux-persist';
 
-const rootReducer = combineReducers({});
-
 const persistConfig = {
   key: 'root',
   storage,
   whitelist: [],
 };
 
+const rootReducer = combineReducers({});
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+export type RootState = ReturnType<typeof rootReducer>;
 export default persistedReducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,4 @@
-import { applyMiddleware } from 'redux';
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import {
   persistStore,
   FLUSH,
@@ -10,36 +9,20 @@ import {
   REGISTER,
 } from 'redux-persist';
 import logger from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import persistedReducer from './rootReducer';
-
-// const middlewares = [
-//   getDefaultMiddleware({
-//     logger,
-//     serializableCheck: {
-//       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//     },
-//   }),
-// ];
-// const middlewareEnhancer = applyMiddleware(...middlewares);
-
-// const enhancers = [middlewareEnhancer];
-// const composedEnhancers = composeWithDevTools(...enhancers);
 
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: getDefaultMiddleware({
-    logger,
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
-  enhancers: [composeWithDevTools()],
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(logger),
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
-let persistor = persistStore(store);
+const persistor = persistStore(store);
 
-export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-export default { store, persistor };
+export { store, persistor };
